7763e1dcc01ce57b5c374a8986e2a0021c33357e
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-03-31 21:28:15 -0700

    feat: basic functional forward and back buttons

--- src/index.js
+++ src/index.js
@@ -2,23 +2,71 @@ import "./styles.css";
 
 console.log("testing");
 
-let btn = document.querySelector("button");
+let btnForward = document.querySelector("#btnForward");
 let dog = document.querySelector("#dog");
 let fish = document.querySelector("#fish");
 let cat = document.querySelector("#cat");
-let imgs = document.querySelectorAll("img");
-console.log(imgs);
-btn.addEventListener("click", () => {
-  imgs.forEach((img) => {
-    let currentZ = parseInt(window.getComputedStyle(img).zIndex);
-    console.log(currentZ);
 
-    if (currentZ === 3) {
-      img.style.zIndex = 1;
-    } else if (currentZ === 2) {
-      img.style.zIndex = 3;
-    } else if (currentZ === 1) {
-      img.style.zIndex = 2;
-    }
-  });
+let imgs = document.querySelectorAll("img");
+const imgsArr = new Array(imgs);
+console.log(imgs);
+console.log(imgsArr);
+let arr = [];
+imgs.forEach((img) => arr.push(img));
+
+const moveNext = (arr) => {
+  let indxTop = arr.findIndex(
+    (img) => window.getComputedStyle(img).zIndex === "2"
+  );
+  console.log(indxTop);
+
+  imgs[indxTop].style.zIndex = 1;
+
+  if (indxTop === 2) {
+    imgs[0].style.zIndex = 2;
+  } else {
+    imgs[indxTop + 1].style.zIndex = 2;
+  }
+};
+
+const moveBack = (arr) => {
+  let indxTop = arr.findIndex(
+    (img) => window.getComputedStyle(img).zIndex === "2"
+  );
+  console.log(indxTop);
+  imgs[indxTop].style.zIndex = 1;
+
+  if (indxTop === 0) {
+    imgs[2].style.zIndex = 2;
+  } else {
+    imgs[indxTop - 1].style.zIndex = 2;
+  }
+};
+
+let clickCount = 0;
+
+btnForward.addEventListener("click", () => {
+  moveNext(arr);
+  //   console.log(imgs[clickCount]);
+  //   const imgToTop = imgs[clickCount];
+  //   imgToTop.style.zIndex = 2;
+  //   imgs.forEach((img) => {
+  //     if (img !== imgs[clickCount]) img.style.zIndex = 1;
+  //   });
+  //   clickCount++;
+  //   if (clickCount === 3) clickCount = 0;
+});
+
+const btnBack = document.querySelector("#btnBackward");
+let clickCountBack = 2 - 1;
+btnBack.addEventListener("click", () => {
+  moveBack(arr);
+  //   console.log(imgs[clickCountBack]);
+  //   const imgToTop = imgs[clickCountBack];
+  //   imgToTop.style.zIndex = 2;
+  //   imgs.forEach((img) => {
+  //     if (img !== imgs[clickCountBack]) img.style.zIndex = 1;
+  //   });
+  //   clickCountBack--;
+  //   if (clickCountBack === -1) clickCountBack = 2;
 });
